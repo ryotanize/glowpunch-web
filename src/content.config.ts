c80e312ef9d12e5eefb78fbfbc9fe54cc8e8a91a
ryotanize <ryotanize@users.noreply.github.com>
2026-04-13 00:02:09 +0900

    Initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
